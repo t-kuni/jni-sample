b0918ccc7e7838a8d546d2cb5d389e90404b751e
tig <tig@example.com>
2017-01-20 14:51:43 +0900

    get vm

--- libHelloWorld.cpp
+++ libHelloWorld.cpp
@@ -1,5 +1,8 @@
+#include <pthread.h>
 #include <stdio.h>
 #include "HelloWorld.h"
+
+static JavaVM *savedVm;
 
 JNIEXPORT void JNICALL
 Java_HelloWorld_print(JNIEnv *env, jobject obj)
@@ -7,4 +10,17 @@ Java_HelloWorld_print(JNIEnv *env, jobject obj)
     jint version = env->GetVersion();
     printf("Hello World! JNI(version:%d)\n", version);
     return;
+}
+
+jint JNI_OnLoad(JavaVM *vm, void *aa) {
+  savedVm = vm;
+  printf("Called JNI_OnLoad\n");
+	// JNIEnv *env;
+	// if (vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) != JNI_OK) {
+	// 	return JNI_ERR;
+	// }
+	// // save and hold pointer to JavaVM
+	// savedVm = vm;
+	// // register native functions and/or initialize native codes here if you need
+	return JNI_VERSION_1_6;
 }
