8689262a6499a914d68d474509a8df3cc2eede0c
kunita <kunita@fernet.co.jp>
2017-01-20 15:01:05 +0900

    上長な記述を削除

--- libHelloWorld.cpp
+++ libHelloWorld.cpp
@@ -4,37 +4,29 @@
 #include "HelloWorld.h"
 #define NUM_THREADS	5
 
+struct _Block {
+  char *name;
+  jstring jname;
+} Block;
+
 static JavaVM *savedVm;
 
 void *PrintHello(void *threadid)
 {
-   long tid;
-   tid = (long)threadid;
-   printf("Hello World! It's me, thread #%ld!\n", tid);
+   printf("called other thread\n");
    pthread_exit(NULL);
 }
 
 JNIEXPORT void JNICALL
 Java_HelloWorld_print(JNIEnv *env, jobject obj)
 {
-    jint version = env->GetVersion();
-    printf("Hello World! JNI(version:%d)\n", version);
-
-    pthread_t threads[NUM_THREADS];
-    int rc;
-    long t;
-    for(t=0;t<NUM_THREADS;t++){
-      printf("In main: creating thread %ld\n", t);
-      rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
-      if (rc){
-        printf("ERROR; return code from pthread_create() is %d\n", rc);
-        exit(-1);
-        }
-      }
+    pthread_t t;
+    printf("In main: creating thread %ld\n", t);
+    pthread_create(&t, NULL, PrintHello, NULL);
 
     /* Last thing that main() should do */
     pthread_exit(NULL);
-     return;
+    return;
 }
 
 jint JNI_OnLoad(JavaVM *vm, void *aa) {
