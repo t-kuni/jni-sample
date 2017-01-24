d79dd4936c5e78729e93aa1ddebcc1029538529e
kunita <kunita@fernet.co.jp>
2017-01-24 17:33:38 +0900

    Revert "文字列の２次元配列をStringの配列に変換"
    This reverts commit 7d7e701470db8c5526c4c38cb31ad0cb5d8f3c53.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,9 @@
-
 class HelloWorld
 {
     private native void print();
     public static void main(String[] args)
     {
         new HelloWorld().print();
-        for (int i = 0; i < mdnNormal.length; i++) {
-          System.out.println("mdnNormal[" + i + "]:" + mdnNormal[i]);
-        }
     }
     static {
         System.loadLibrary("HelloWorld");
@@ -16,6 +12,4 @@ class HelloWorld
     public static int test(int a, int b) {
       return a + b;
     }
-
-    static private String[] mdnNormal;
 }
