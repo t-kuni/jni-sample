c1e65d0103784fe0be220ba7e9868b4fd9612d75
kunita <kunita@fernet.co.jp>
2017-01-27 18:19:43 +0900

    スレッドを最低限動く状態にした

--- libHelloWorld.cpp
+++ libHelloWorld.cpp
@@ -2,101 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "HelloWorld.h"
+#include <unistd.h>
 #define NUM_THREADS  5
-
-typedef struct {
-  char *name;
-  jstring jname;
-} Block;
-
-static JavaVM *savedVm;
-
-// get saved JavaVM
-JavaVM *getVM() {
-  return savedVm;
-}
-
-// get JNIEnv on current thread
-// you should call AttachCurrentThread/AttachCurrentThreadAsDaemon on the thread
-// before this function call
-JNIEnv *getEnv() {
-  JNIEnv *env = NULL;
-  if (savedVm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) != JNI_OK) {
-    env = NULL;
-  }
-  return env;
-}
 
 void *PrintHello(void *arg)
 {
-  printf("thread 1\n");
+  const char *msg = (const char *) arg;
 
-  JavaVM *vm = getVM();
-  if (vm) {
-    printf("thread 2\n");
-    JNIEnv *env;
+  printf("thread run[name:%s]\n", msg);
 
-    vm->AttachCurrentThread((void **)&env, NULL);
+  sleep(4);
 
-    env->FindClass("HelloWorld");
-    env->FindClass("HelloWorld");
-
-    Block *b = (Block *) arg;
-    printf("b.name:%s\n", b->name);
-    printf("b.jname:%s\n", env->GetStringUTFChars(b->jname, NULL));
-
-    jclass cls = env->FindClass("HelloWorld");
-    jmethodID methodID = env->GetStaticMethodID(cls, "test", "(II)I");
-    jint val = env->CallStaticIntMethod(cls, methodID, 3, 5);
-    printf("call test = %d\n", val);
-
-    vm->DetachCurrentThread();
-  } else {
-    printf("nothing vm\n");
-  }
-
-  printf("thread 3\n");
+  printf("thread end\n");
 
   pthread_exit(NULL);
+}
+
+void run(pthread_mutex_t *mutex, const char *msg) {
+    pthread_t t;
+    pthread_create(&t, NULL, PrintHello, (void *)msg);
 }
 
 JNIEXPORT void JNICALL
 Java_HelloWorld_print(JNIEnv *env, jobject obj)
 {
-    Block *b = (Block *) malloc(sizeof(Block));
-    b->name = "aaaaaaaa";
-    b->jname = (jstring) env->NewGlobalRef(env->NewStringUTF("bbbbbbb"));
+    printf("main start\n");
 
-    printf("main 1\n");
+    static pthread_mutex_t mutex;
 
-    jclass cls = env->FindClass("HelloWorld");
-    jmethodID methodID = env->GetStaticMethodID(cls, "test", "(II)I");
-    jint val = env->CallStaticIntMethod(cls, methodID, 3, 5);
-    printf("call test = %d\n", val);
-
-    pthread_t t;
-    int r;
-    r = pthread_create(&t, NULL, PrintHello, (void *)b);
-    if (r) {
-      printf("thread was't created");
-    }
-
-    printf("main 2\n");
+    run(&mutex, "A");
+    sleep(5);
 
     pthread_exit(NULL);
 
-    printf("main 3\n");
-    free(b);
+    printf("main end\n");
     return;
-}
-
-jint JNI_OnLoad(JavaVM *vm, void *aa) {
-  JNIEnv *env;
-  if (vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) != JNI_OK) {
-    return JNI_ERR;
-  }
-  // save and hold pointer to JavaVM
-  savedVm = vm;
-  // register native functions and/or initialize native codes here if you need
-  return JNI_VERSION_1_6;
 }
